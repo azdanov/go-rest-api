c8367334822aa634f9e15abf30b390ef5a327bb3
Anton Ždanov <anton@azdanov.dev>
2025-04-19 12:17:52 +0300

    add comment service

---
+++ internal/comment/comment.go
@@ -0,0 +1,54 @@
+package comment
+
+import (
+	"context"
+	"errors"
+	"log"
+)
+
+var (
+	ErrCommentNotFound = errors.New("comment not found")
+	ErrNotImplemented  = errors.New("not implemented")
+)
+
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+type Service struct {
+	Store Store
+}
+
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) CreateComment(ctx context.Context, comment Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	comment, err := s.Store.GetComment(ctx, id)
+	if err != nil {
+		log.Println(err)
+		return Comment{}, ErrCommentNotFound
+	}
+	return comment, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, comment Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
